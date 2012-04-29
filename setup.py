0b19036f47e58c3a0dc0a28fddbb882bcab2474b
Gavin M. Roy <gmr@myyearbook.com>
2012-04-29 00:10:22 -0400

    Remove the non-needed dependency and bump the minor rev

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='clihelper',
-      version='1.0.0',
+      version='1.0.1',
       description='Internal Command-Line Application Wrapper',
       long_description=('clihelper is a wrapper for command-line daemons '
                         'providing a core Controller class and methods for '
