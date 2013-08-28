d0191c43c784b229ce104700989dfb91c67ec490
Gavin M. Roy <gmr@meetme.com>
2013-08-28 09:31:01 -0400

    Raise a NotImplementedError for Windows

--- helper/windows.py
+++ helper/windows.py
@@ -2,7 +2,6 @@
 Windows platform support for running the application as a detached process.
 
 """
-import multiprocessing
 import subprocess
 import sys
 
@@ -10,23 +9,14 @@ DETACHED_PROCESS = 8
 
 
 class Daemon(object):
+    """Daemonize the helper application, putting it in a forked background
+    process.
 
-    def __init__(self, controller, user=None, group=None,
-                 pid_file=None, prevent_core=None, exception_log=None):
-        """Daemonize the controller, optionally passing in the user and group
-        to run as, a pid file, if core dumps should be prevented and a path to
-        write out exception logs to.
-
-        :param helper.Controller controller: The controller to daaemonize & run
-        :param str user: Optional username to run as
-        :param str group: Optional group to run as
-        :param str pid_file: Optional path to the pidfile to run
-        :param bool prevent_core: Don't make any core files
-        :param str exception_log: Optional exception log path
-
-        """
-        args = [sys.executable]
-        args.extend(sys.argv)
-        self.pid = subprocess.Popen(args,
-                                    creationflags=DETACHED_PROCESS,
-                                    shell=True).pid
+    """
+    def __init__(self, controller):
+        raise NotImplementedError
+        #args = [sys.executable]
+        #args.extend(sys.argv)
+        #self.pid = subprocess.Popen(args,
+        #                            creationflags=DETACHED_PROCESS,
+        #                            shell=True).pid
